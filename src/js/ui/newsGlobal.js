6e79a0a601ba043e141f32dff1b513577c7e8dcd
JcFiscus <129557433+JcFiscus@users.noreply.github.com>
2025-08-14 15:39:32 -0500

    Update newsGlobal.js

--- src/js/ui/newsGlobal.js
+++ src/js/ui/newsGlobal.js
@@ -1,4 +1,11 @@
-// global feed
-export function renderGlobalNews(feed) {
-  // TODO: implement global news feed
+export function initGlobalFeed(feedEl){
+  function log(msg){
+    const line = document.createElement('div');
+    line.className = 'line';
+    const ts = new Date().toLocaleTimeString();
+    line.textContent = `[${ts}] ${msg}`;
+    feedEl.prepend(line);
+    while (feedEl.childElementCount > 200) feedEl.lastElementChild.remove();
+  }
+  return { log };
 }
